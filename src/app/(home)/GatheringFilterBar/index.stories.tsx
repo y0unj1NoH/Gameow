e316ae8c2cb2bef4aa256dcf0122888bc29eb8a0
y0unj1NoH <nohyoong711@gmail.com>
2025-12-09 21:57:57 +0900

    Chore: 임시 주석 처리

--- src/app/(home)/GatheringFilterBar/index.stories.tsx
+++ src/app/(home)/GatheringFilterBar/index.stories.tsx
@@ -1,25 +1,26 @@
-import ModalContainer from '@/components/commons/ModalContainer';
-import { ModalStoreProvider } from '@/providers/ModalProvider';
-import type { Meta, StoryObj } from '@storybook/nextjs';
-import GatheringFilterBar from '.';
+// import ModalContainer from '@/components/commons/ModalContainer';
+// import { ModalStoreProvider } from '@/providers/ModalProvider';
+// import type { Meta, StoryObj } from '@storybook/nextjs';
+// import GatheringFilterBar from '.';
 
-const meta: Meta<typeof GatheringFilterBar> = {
-	title: 'Home/GatheringFilterBar',
-	tags: ['autodocs']
-};
+// const meta: Meta<typeof GatheringFilterBar> = {
+// 	title: 'Home/GatheringFilterBar',
+// 	tags: ['autodocs']
+// };
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+// export default meta;
+// type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-	render: () => (
-		<ModalStoreProvider>
-			<GatheringFilterBar
-				setFilterCriteria={criteria => {
-					console.log('🔍 Filter criteria changed:', criteria);
-				}}
-			/>
-			<ModalContainer />
-		</ModalStoreProvider>
-	)
-};
+// export const Default: Story = {
+// 	render: () => (
+// 		<ModalStoreProvider>
+// 			<GatheringFilterBar
+// 				setFilterCriteria={criteria => {
+// 					console.log('🔍 Filter criteria changed:', criteria);
+// 				}}
+// 			/>
+// 			<ModalContainer />
+// 		</ModalStoreProvider>
+// 	)
+// };
+export default {};
